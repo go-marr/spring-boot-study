37e51d711c2a4a9a14146763a26c4c74870bec3a
gomar <ju79318@naver.com>
2024-08-02 18:21:01 +0900

    service 에서 controller 로 옮김

--- src/main/java/com/rubypaper/study1/api/BoardApiController.java
+++ src/main/java/com/rubypaper/study1/api/BoardApiController.java
@@ -2,39 +2,30 @@ package com.rubypaper.study1.api;
 
 import com.rubypaper.study1.domain.Board;
 import com.rubypaper.study1.dto.BoardDTO;
-import com.rubypaper.study1.dto.BoardResponseDTO;
 import com.rubypaper.study1.repository.BoardRepository;
-import com.rubypaper.study1.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.border.Border;
 import java.util.List;
 
 @RestController
 public class BoardApiController {
-
-    @Autowired
-    private BoardService boardService;
     @Autowired
     private BoardRepository boardRepository;
 
     @GetMapping("/board")
     public ResponseEntity<List<Board>> getBoard(){
-        return new ResponseEntity(boardRepository.findAll(),HttpStatus.OK);
+        return new ResponseEntity<>(boardRepository.findAll(),HttpStatus.OK);
     }
 
     @PostMapping("/board")
     public ResponseEntity<Board> createBoard(@RequestBody  BoardDTO boardDTO){
         Board board = boardDTO.toEntity();
         boardRepository.save(board);
-
-        return (board != null) ?
-                new ResponseEntity<>(board, HttpStatus.OK) :
-                new ResponseEntity<>(board, HttpStatus.BAD_GATEWAY);
+        return new ResponseEntity<>(board,HttpStatus.OK);
     }
 
     @DeleteMapping("/board/{id}")
--- src/main/java/com/rubypaper/study1/controller/UserController.java
+++ src/main/java/com/rubypaper/study1/controller/UserController.java
@@ -4,15 +4,15 @@ import com.rubypaper.study1.domain.User;
 import com.rubypaper.study1.dto.UserDTO;
 import com.rubypaper.study1.dto.ResponseDTO;
 
-import com.rubypaper.study1.service.UserService;
+import com.rubypaper.study1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -20,31 +20,45 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
 
     @PostMapping("/users")
+    @Transactional
     public ResponseEntity<ResponseDTO> saveUser(@RequestBody UserDTO userDTO){
+        if(!userDTO.getEmail().contains("@"))
+            return  new ResponseEntity<>(new ResponseDTO("@ 없음"), HttpStatus.BAD_GATEWAY);
         User user = userDTO.toEntity();
-        log.info(user.toString());
-       return userService.save(user);
+        userRepository.save(user);
+        return new ResponseEntity<>(new ResponseDTO("저장"), HttpStatus.OK);
     }
 
 
     @DeleteMapping("/users/{id}")
+    @Transactional
     public ResponseEntity<ResponseDTO> deleteUser(@PathVariable Long id){
-        return  userService.delete(id);
+        User findUser = userRepository.findById(id).orElse(null);
+        userRepository.delete(findUser);
+        return new ResponseEntity<>(new ResponseDTO("삭제"),HttpStatus.OK);
     }
 
 
     @PatchMapping("/users/{id}")
+    @Transactional
     public ResponseEntity<ResponseDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
-        return userService.update(id,userDTO);
+        User findUser = userRepository.findById(id).orElse(null);
+
+        findUser.setName(userDTO.getName());
+        findUser.setEmail(userDTO.getEmail());
+        findUser.setPhone(userDTO.getPhone());
+        return new ResponseEntity<>(new ResponseDTO("수정"),HttpStatus.OK);
+
     }
 
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return userService.findAll();
+    @Transactional
+    public ResponseEntity<List<User>> getUsers(){
+        return new ResponseEntity<>(userRepository.findAll(),HttpStatus.OK);
     }
 
 }
