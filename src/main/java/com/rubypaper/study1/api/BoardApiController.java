23a41311a4090dd318668800367ac07b8be50ba5
gomar <ju79318@naver.com>
2024-08-05 13:54:16 +0900

    BoardApiController 리팩토링

--- src/main/java/com/rubypaper/study1/api/BoardApiController.java
+++ src/main/java/com/rubypaper/study1/api/BoardApiController.java
@@ -3,6 +3,7 @@ package com.rubypaper.study1.api;
 import com.rubypaper.study1.domain.Board;
 import com.rubypaper.study1.dto.BoardDTO;
 import com.rubypaper.study1.repository.BoardRepository;
+import com.rubypaper.study1.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,36 +19,35 @@ import java.util.List;
 public class BoardApiController {
     @Autowired
     private BoardRepository boardRepository;
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board")
-    public ResponseEntity<List<Board>> getBoard(){
-        return new ResponseEntity<>(boardRepository.findAll(),HttpStatus.OK);
+    public ResponseEntity<List<Board>> getBoards(){
+        return boardService.getBoards();
     }
 
     @GetMapping("/board/list")
-    public ResponseEntity<Page<Board>> getBoard(@RequestParam(value="page",defaultValue = "0") int page){
-        return new ResponseEntity<>(this.boardRepository.findAll(PageRequest.of(page,5)),HttpStatus.OK);
+    public ResponseEntity<Page<Board>> getListBoards(@RequestParam(value="page",defaultValue = "0") int page){
+        return boardService.getListBoards(page);
     }
 
     @PostMapping("/board")
     public ResponseEntity<Board> createBoard(@RequestBody  BoardDTO boardDTO){
         Board board = boardDTO.toEntity();
-        boardRepository.save(board);
-        return new ResponseEntity<>(board,HttpStatus.OK);
+        return boardService.save(board);
     }
 
     @DeleteMapping("/board/{id}")
     public ResponseEntity<Board> deleteBoard(@PathVariable Long id){
         Board findBoard = boardRepository.findById(id).orElse(null);
-        boardRepository.delete(findBoard);
-        return new ResponseEntity<>(findBoard,HttpStatus.OK);
+        return boardService.delete(findBoard);
     }
-    @Transactional
-    @PostMapping("board/{id}")
+
+    @PostMapping("/board/{id}")
     public ResponseEntity<Board> updateBoard(@PathVariable Long id,@RequestBody BoardDTO boardDTO){
         Board findBoard = boardRepository.findById(id).orElse(null);
-        findBoard.updateBoard(boardDTO.getTitle(),boardDTO.getContent(),boardDTO.getAuthor());
-        return new ResponseEntity<>(findBoard,HttpStatus.OK);
+        return boardService.update(findBoard,boardDTO);
 
     }
 }
