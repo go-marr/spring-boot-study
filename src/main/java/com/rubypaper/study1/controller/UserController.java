e084a43233a40c4cd17744561d0689e3a503f21a
gomar <ju79318@naver.com>
2024-08-05 13:36:29 +0900

    UserController 리팩토링

--- src/main/java/com/rubypaper/study1/controller/UserController.java
+++ src/main/java/com/rubypaper/study1/controller/UserController.java
@@ -5,10 +5,10 @@ import com.rubypaper.study1.dto.UserDTO;
 import com.rubypaper.study1.dto.ResponseDTO;
 
 import com.rubypaper.study1.repository.UserRepository;
+import com.rubypaper.study1.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -21,16 +21,14 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
 
     @PostMapping("/users")
     @Transactional
     public ResponseEntity<ResponseDTO> saveUser(@RequestBody UserDTO userDTO){
-        if(!userDTO.getEmail().contains("@"))
-            return  new ResponseEntity<>(new ResponseDTO("@ 없음"), HttpStatus.BAD_GATEWAY);
-        User user = userDTO.toEntity();
-        userRepository.save(user);
-        return new ResponseEntity<>(new ResponseDTO("저장"), HttpStatus.OK);
+        return userService.save(userDTO);
     }
 
 
@@ -38,27 +36,20 @@ public class UserController {
     @Transactional
     public ResponseEntity<ResponseDTO> deleteUser(@PathVariable Long id){
         User findUser = userRepository.findById(id).orElse(null);
-        userRepository.delete(findUser);
-        return new ResponseEntity<>(new ResponseDTO("삭제"),HttpStatus.OK);
+        return userService.delete(findUser);
     }
-
 
     @PatchMapping("/users/{id}")
     @Transactional
     public ResponseEntity<ResponseDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
         User findUser = userRepository.findById(id).orElse(null);
-
-        findUser.setName(userDTO.getName());
-        findUser.setEmail(userDTO.getEmail());
-        findUser.setPhone(userDTO.getPhone());
-        return new ResponseEntity<>(new ResponseDTO("수정"),HttpStatus.OK);
-
+        return userService.update(findUser,userDTO);
     }
 
     @GetMapping("/users")
     @Transactional
     public ResponseEntity<List<User>> getUsers(){
-        return new ResponseEntity<>(userRepository.findAll(),HttpStatus.OK);
+        return userService.getUsers();
     }
 
 }
